07f912bdb825205e0e9be470abb9e133cf785549
Emiliano Cervantes <ovman96@hotmail.com>
2017-11-15 13:21:31 -0600

    Juego de BrickBreaker en Unity

---
+++ Brick Breaker/Assets/Scrips/BarraJugador.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BarraJugador : MonoBehaviour {
+
+	public float velocidad = 1f;
+
+	private Vector3 posicion = new Vector3 (0, 0.4f, -3f);
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float posicionX = transform.position.x + (Input.GetAxis ("Horizontal")*velocidad);
+		posicion = new Vector3 (Mathf.Clamp (posicionX, -4f, 4f), 0.4f, -3f);
+		transform.position = posicion;
+	}
+}
---
+++ NewBrickBreaker/Assets/_Scripts/Barra.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Barra : MonoBehaviour {
+
+	Vector2 posicion;
+	public float velocidad;
+	// Use this for initialization
+	void Start () {
+		posicion = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
